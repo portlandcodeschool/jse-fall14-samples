ca2b83ed00e1ac5b1f78e6adae57d335bf9ee825
Benjamin P. Verble <bvrider@gmail.com>
2014-10-22 17:16:20 -0700

    add oct22 samples

---
+++ backbone-client-only/Oct22/4-refactor-namespace/js/main.js
@@ -0,0 +1,62 @@
+// challenge: refactor this application to have only 
+// one variable besides your dependencies
+// $, _, and Backbone are already globals
+
+var Todo = Backbone.Model.extend({});
+
+var Todos = Backbone.Collection.extend({
+  model: Todo,
+  comparator: 'cid' // or perhaps sort by 'title'
+});
+
+var todos = new Todos();
+
+todos.add([
+  { title: 'mow the lawn',
+    description: 'fill the gasoline tank, start the engine, cut all the grass, bag grass'},
+  { title: 'paint the house',
+    description: 'paint all the things'},
+  { title: 'fix the leaky bathtub faucet',
+    description: 'get the seat wrench, turn off water main, unscrew things, get new parts'}
+]);
+
+var TodoInputView = Backbone.View.extend({
+  el: '#todo-form',
+  events: {
+    'click #add-todo': 'addTodo'
+  },
+  addTodo: function (event) {
+    event.preventDefault();
+    var $todoInput = $(this.el).find('#todo-input');
+    var $description = $(this.el).find('#description-input'); 
+    console.log('button was clicked');
+    var todoInput = $todoInput.val();
+    var descriptionInput = $description.val();
+    this.collection.add({title: todoInput, description: descriptionInput});
+    $description.val('');
+    $todoInput.val('');
+  }
+});
+
+var TodoListView = Backbone.View.extend({
+  el: '#todo-list',
+  initialize: function () {
+    this.collection.on('add', this.render, this);
+  },
+  render: function () {
+    var outputHtml = '';
+
+    this.collection.models.forEach(function (item) { 
+      outputHtml += '<li><strong>' + item.get('title') + ':  </strong>' +
+                     item.get('description') + '</li>';
+    });
+
+    $(this.el).html(outputHtml);
+  }
+});
+
+$(function () {
+  var todoInputView = new TodoInputView({collection: todos});
+  todoListView = new TodoListView({collection: todos});
+  todoListView.render();
+});
